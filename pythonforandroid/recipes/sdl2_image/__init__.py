bf3430e344b9bdb7a93164f14a0a36acdd1f3868
Mirko Galimberti <me@mirkogalimberti.com>
2022-09-07 16:06:00 +0200

    Save previous

--- pythonforandroid/recipes/sdl2_image/__init__.py
+++ pythonforandroid/recipes/sdl2_image/__init__.py
@@ -2,13 +2,17 @@ from pythonforandroid.recipe import BootstrapNDKRecipe
 
 
 class LibSDL2Image(BootstrapNDKRecipe):
-    version = '2.0.4'
-    url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
+    version = '2.6.2'
+    url = 'https://github.com/libsdl-org/SDL_image/releases/download/release-{version}/SDL2_image-{version}.tar.gz'
     dir_name = 'SDL2_image'
 
-    patches = ['toggle_jpg_png_webp.patch',
-               'extra_cflags.patch',
-               ]
+    # patches = ['extra_cflags.patch']
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True, with_python=False):
+        env = super().get_recipe_env(arch, with_flags_in_cc, with_python)
+        env['SUPPORT_JPG'] = True
+        env['SUPPORT_PNG'] = True
+        env['SUPPORT_WEBP'] = True
 
 
 recipe = LibSDL2Image()
